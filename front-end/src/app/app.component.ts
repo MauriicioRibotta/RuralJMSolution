9ed7b13b66c426c0668aa2b87a0c6dd3ba908749
Mauri Ribotta <mauriribotta2@gmail.com>
2026-02-10 17:43:21 -0300

    feat: update top menu navigation with correct registration and admin links

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -10,8 +10,8 @@ import { RouterOutlet, RouterLink } from '@angular/router';
       <div class="container mx-auto flex justify-between items-center">
         <a routerLink="/" class="text-xl font-bold hover:text-gray-300">RuralIA</a>
         <div class="space-x-4">
-          <a routerLink="/animals/new" class="hover:text-blue-300 transition">Inscribir</a>
-          <a routerLink="/animals" class="hover:text-green-300 transition">Listado</a>
+          <a routerLink="/inscripcion/expositor" class="hover:text-blue-300 transition">Inscripción</a>
+          <a routerLink="/admin" class="hover:text-red-300 transition">Admin</a>
         </div>
       </div>
     </nav>
